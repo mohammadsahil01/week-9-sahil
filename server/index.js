28b8ad91ab65fd51ed2321c5ea802555603c17d6
mohammadsahil01 <mohammadsahil01@gmail.com>
2023-08-10 02:36:01 +0530

    removed comment

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use("/user", userRouter)
 
 
 // Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
+
 mongoose.connect('mongodb+srv://mohammadsahil01:khan2580@cluster1.eqztuej.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "cluster1" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
